d83567411f2e07a78d38ba7d12dc0bfdd652472d
facubardanca <facundobardanca@gmail.com>
2025-05-19 14:41:27 -0300

    Add files via upload

---
+++ netlify/functions/enviarCorreo.js
@@ -0,0 +1,50 @@
+const mailjet = require('mailjet').connect(
+  process.env.MJ_APIKEY_PUBLIC,
+  process.env.MJ_APIKEY_PRIVATE
+);
+
+exports.handler = async function (event, context) {
+  const data = JSON.parse(event.body);
+  const { energia, sueno, historia, dia, email } = data;
+
+  const message = `
+    ✉️ Nueva respuesta desde la web:
+
+    🔋 Energía deseada: ${energia}
+    🌌 Sueño profundo: ${sueno}
+    📖 Historia personal: ${historia}
+    📅 Día perfecto: ${dia}
+    📩 Email del usuario: ${email}
+  `;
+
+  try {
+    await mailjet.post('send', { version: 'v3.1' }).request({
+      Messages: [
+        {
+          From: {
+            Email: "facundobardanca@gmail.com",
+            Name: "Nueva Experiencia"
+          },
+          To: [
+            {
+              Email: "facundobardanca@gmail.com",
+              Name: "Facundo"
+            }
+          ],
+          Subject: "Nueva Experiencia - Respuesta desde la web",
+          TextPart: message
+        }
+      ]
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true })
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
